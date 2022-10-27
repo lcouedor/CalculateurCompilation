93f48f05cce9946248a4e69d7a1b88ca2e5b1b8b
lcouedor <leo.couedor@gmail.com>
2022-10-27 16:38:49 +0200

    micro modif

--- script.sh
+++ script.sh
@@ -3,18 +3,18 @@ rm sym*.*
 rm Yylex*.*
 rm CUP*
 
-clear
+# clear
 
-LEX="ex.flex"
-CUP="ex.cup"
-ENTREE="entree"
-LOCATION="/Users/leocouedor/Documents/ressources/java-cup-v11a.jar java_cup.Main"
+# LEX="ex.flex"
+# CUP="ex.cup"
+# ENTREE="entree"
+# LOCATION="/Users/leocouedor/Documents/ressources/java-cup-v11a.jar java_cup.Main"
 
-jflex $LEX
-#java -cp /Users/leocouedor/Documents/ressources/java-cup-v11a.jar java_cup.Main -dump $CUP
-java -cp $LOCATION $CUP
-javac -nowarn parser.java sym.java Yylex.java
+# jflex $LEX
+# #java -cp /Users/leocouedor/Documents/ressources/java-cup-v11a.jar java_cup.Main -dump $CUP
+# java -cp $LOCATION $CUP
+# javac -nowarn parser.java sym.java Yylex.java
 
-clear
+# clear
 
-java parser < $ENTREE
+# java parser < $ENTREE
